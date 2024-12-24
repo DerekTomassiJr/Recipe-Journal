8613327ed3e8add7a958f98f0768eb5b4179cde0
derektomassijr <derektomassi@me.com>
2024-12-24 14:36:18 -0500

    RJP-6: Initial Creation of Recipe Page

---
+++ src/pages/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Recipe.css';
+import recipe_image from '../assets/penne.jpg';
+
+function Recipe() {
+    return (
+        <div className='recipe-window-background'>
+            <h1 id="Recipe-Name">Penne alla Vodka</h1>
+        </div>
+    );
+}
+
+export default Recipe
